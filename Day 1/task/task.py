8622ffea8bf2df448b9ef56ce7a8d9dd9df1dc92
bhg995 <bhg995@myy.haaga-helia.fi>
2024-12-13 15:32:19 +0200

    First day

---
+++ Day 1/task/task.py
@@ -0,0 +1,14 @@
+# Write your code below this line 👇
+yourName = input("What is your name? ")
+print(f'Your name is {yourName}, and the length of your name: {len(yourName)}')
+# 2
+print(len(input("What is your address? ")))
+# 3
+username = input("What is your username? ")
+length = len(username)
+print(length)
+
+city = input("What's the name of the city you grew up in? ")
+pet = input("What's your pets name? What would you call a pet of yours? ")
+
+print(f"Your band name could be {city} {pet}.")
